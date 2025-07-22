ec3710572da732b2bb998cbd85e107642ecdfdc2
Pokaneliot <aneliotramamonjisoa@gmail.com>
2025-07-22 16:42:22 +0300

    Api for sending and receiving data for service-order

---
+++ src/DTO/ServiceOrder/CreateServiceOrderDTO.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DTO\ServiceOrder;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Enum\Status;
+use App\DTO\SecuredZone\CreateSecuredZoneDTO;
+
+class CreateServiceOrderDTO
+{
+    #[Assert\Length(
+        max: 1000,
+        maxMessage: 'La description ne peut pas dépasser {{ limit }} caractères'
+    )]
+    public ?string $description = null;
+
+    #[Assert\NotBlank(message: 'L\'ID du client est requis')]
+    public string $clientId;
+
+    /**
+     * Données de la zone sécurisée à créer avec la commande
+     */
+    #[Assert\NotBlank(message: 'Les données de la zone sécurisée sont requises')]
+    #[Assert\Valid]
+    public CreateSecuredZoneDTO $securedZone;
+}
---
+++ src/DTO/ServiceOrder/ServiceOrderDTO.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DTO\ServiceOrder;
+
+use App\DTO\SecuredZone\SecuredZoneDTO;
+use App\Enum\Status;
+use DateTimeImmutable;
+
+class ServiceOrderDTO
+{
+    public function __construct(
+        public string $encryptedId,
+        public ?string $description,
+        public Status $status,
+        public DateTimeImmutable $createdAt,
+        public SecuredZoneDTO $securedZone,
+        public string $clientId,
+        public string $clientName,
+    ) {}
+}
