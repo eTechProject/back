6411a12efd01f515b4a82326e3ef9363eac68e2e
Pokaneliot <aneliotramamonjisoa@gmail.com>
2025-07-25 16:11:02 +0300

    User Role set on payload

--- src/EventListener/JWTCreatedListener.php
+++ src/EventListener/JWTCreatedListener.php
@@ -1,16 +1,28 @@
 <?php
 namespace App\EventListener;
 
+use App\Entity\User;
+use App\Service\CryptService;
+use App\Enum\EntityType;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
 
 class JWTCreatedListener
 {
+    public function __construct(
+        private CryptService $cryptService
+    ) {}
+
     public function onJWTCreated(JWTCreatedEvent $event): void
     {
         $user = $event->getUser();
+        
+        if (!$user instanceof User) {
+            return;
+        }
+        
         $payload = $event->getData();
-        $payload['id'] = $user->getId();
-        $payload['roleType'] = $user->getRole();
+        $payload['id'] = $this->cryptService->encryptId($user->getId(), EntityType::USER->value);
+        $payload['roleType'] = $user->getRole()->value;
         $event->setData($payload);
     }
 }
