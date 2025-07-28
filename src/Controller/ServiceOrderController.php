dee9ca0c8c5d6e82f9013431b88f26f941183903
Pokaneliot <aneliotramamonjisoa@gmail.com>
2025-07-28 14:42:03 +0300

    fix endpoint

--- src/Controller/ServiceOrderController.php
+++ src/Controller/ServiceOrderController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-#[Route('/api/public/service-orders', name: 'api_service_orders_')]
+#[Route('/api/service-orders', name: 'api_service_orders_')]
 class ServiceOrderController extends AbstractController
 {
     public function __construct(
