16b1c397ee8d38769d1deaee06ef2a0807654e5d
Lova <2niaintsoa@gmail.com>
2025-08-05 16:23:59 +0300

    feat: Add agent profile management endpoints

--- src/DTO/Agent/Request/UpdateAgentDTO.php
+++ src/DTO/Agent/Request/UpdateAgentDTO.php
@@ -14,4 +14,24 @@ class UpdateAgentDTO
 
     #[Assert\Url(message: 'L\'URL de la photo n\'est pas valide')]
     public ?string $profilePictureUrl;
+
+    #[Assert\Length(
+        min: 2,
+        max: 255,
+        minMessage: 'Le nom doit contenir au moins {{ limit }} caractères',
+        maxMessage: 'Le nom ne peut pas dépasser {{ limit }} caractères'
+    )]
+    public ?string $name;
+
+    #[Assert\Regex(
+        pattern: '/^\+?[0-9\s\-\(\)]{7,}$/',
+        message: 'Le format du numéro de téléphone n\'est pas valide'
+    )]
+    public ?string $phone;
+
+    #[Assert\Length(
+        min: 8,
+        minMessage: 'Le mot de passe doit contenir au moins {{ limit }} caractères'
+    )]
+    public ?string $password;
 }
