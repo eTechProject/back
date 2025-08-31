1f380c25d936f0a08b6e9449bf5cb11e13d80cda
Pokaneliot <aneliotramamonjisoa@gmail.com>
2025-08-31 21:52:21 +0300

    feat: Dashboard client implementation

--- src/DTO/Dashboard/Response/DashboardResponseDTO.php
+++ src/DTO/Dashboard/Response/DashboardResponseDTO.php
@@ -7,6 +7,4 @@ class DashboardResponseDTO
     public array $filters;
     public array $kpis;
     public array $charts;
-    public array $indicators;
-    // Vous pouvez typer plus finement avec les DTO internes si besoin
 }
--- src/Repository/AgentLocationsArchiveRepository.php
+++ src/Repository/AgentLocationsArchiveRepository.php
@@ -11,33 +11,23 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class AgentLocationsArchiveRepository extends ServiceEntityRepository
 {
+    
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, AgentLocationsArchive::class);
     }
+    /**
+     * @param int $taskId
+     * @return AgentLocationsArchive|null
+     */
+    public function findByTaskId(int $taskId): ?AgentLocationsArchive
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.task = :taskId')
+            ->setParameter('taskId', $taskId)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 
-    //    /**
-    //     * @return AgentLocationsArchive[] Returns an array of AgentLocationsArchive objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('a.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
 
-    //    public function findOneBySomeField($value): ?AgentLocationsArchive
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
